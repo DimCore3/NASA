f5c4e01ad667ec7aefeb9aea405fa794b9d4c614
DimCore3 <3dmi3y@gmail.com>
2023-02-15 03:27:57 +0400

    Карусель дней добавлена. Туда сейчас передается массив дат и родительский хук

--- src/components/NearEarthObjects.jsx
+++ src/components/NearEarthObjects.jsx
@@ -1,24 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import DaysCarousel from './UI/DaysCarousel/DaysCarousel'
 import '../styles/NearEarthObjects.scss'
 
 const NearEarthObjects = ({neoWs}) => {
+    const [enabledIndexDay, setEnabledIndexDay] = useState(0);
+    const [dateList, setDateList] = useState([]);
 
     useEffect(() => {
-        getDates(neoWs.near_earth_objects);
+        getInfo(neoWs.near_earth_objects);
     }, [neoWs])
     
-    const getDates = (neoDays) => {
-        for(let day in neoDays) {
-            let date = new Date(day)
-            console.log(date);
+    const getInfo = (neoDays) => {
+        try {
+            const resultDateList = [];
+            console.log(neoDays);
+            for(let day in neoDays) {
+                resultDateList.push(day);
+            }
+            resultDateList.sort((a, b) => {
+                a = new Date(a);
+                b = new Date(b);
+                return a - b
+            });
+            setDateList(resultDateList);
+
+        } catch (error) {
+            console.log(error);
         }
     }
 
     return ( 
         <div className="nearEarthObjects">
-            <DaysCarousel/>
-            Near Earth Object Web Service
+            {dateList.length !== 0 
+                ? <DaysCarousel allDates={dateList} enabledIndexDay={enabledIndexDay} setEnabledIndexDay={setEnabledIndexDay}/>
+                : <h2 className='neoContent'>Загрузка данных...</h2>
+            }
+            <div className='neoContent'>
+                Here will be Neo graphic
+            </div>
         </div>
      );
 }
--- src/components/UI/DaysCarousel/DaysCarousel.jsx
+++ src/components/UI/DaysCarousel/DaysCarousel.jsx
@@ -1,11 +1,36 @@
 import classes from './DaysCarousel.module.scss'
 
-const DaysCarousel = () => {
-    return ( 
+const DaysCarousel = ({ allDates, enabledIndexDay, setEnabledIndexDay }) => {
+
+    const months = [
+        'Jun', 'Feb', 'Mar',
+        'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep',
+        'Nov', 'Dec'
+    ]
+
+    const getDateForSelecter = (day) => {
+        console.log(day)
+        let date = new Date(day);
+        return date.getDate() + ' ' + months[date.getMonth()];
+    }
+
+    return (
         <div className={classes.neoDaysCarousel}>
-            Карусель с днями
+            {allDates.map((day, index) =>
+                <div key={index} className={classes.dayOfCarousel}>
+                    {
+                        <div
+                            className={enabledIndexDay === index ? classes.enabledDay : classes.disabledDay}
+                            onClick={() => setEnabledIndexDay(index)}
+                        >
+                            {getDateForSelecter(day)}
+                        </div>
+                    }
+                </div>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default DaysCarousel;
